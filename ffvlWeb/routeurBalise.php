d50bddcd7cc362032d08d034aed04825dc9365e6
Florentin DUBUC <DubucF@BTS-SN.LRQ>
2020-01-29 10:34:43 +0100

    Updated the API's reponse

--- ffvlWeb/routeurBalise.php
+++ ffvlWeb/routeurBalise.php
@@ -8,18 +8,38 @@
     if($request_method == 'GET'){
         if(!empty($_GET["id"])){       // Si la propriété id existe
             $id = intval($_GET["id"]);
-            $reponse = getBalise($id);    // alors appelle de la méthode getBalise($id)
+            $resultat = getBalise($id);    // alors appelle de la méthode getBalise($id)
     
            // création de la réponse en JSON
            header('Content-Type: application/json');
-           echo json_encode($reponse, JSON_PRETTY_PRINT);
+           if($resultat !=false){
+            $retour['statut'] = 1;
+            $retour['message'] = "GET Balise";
+            $retour['donnees'] = $resultat;
+            
+        } else{
+            $retour['statut'] = 0;
+            $retour['message'] = "Echec de recuparation des donnees";
+            
+        }
+           echo json_encode($retour, JSON_PRETTY_PRINT);
     
         }else{
-            $reponse = getBalises();// alors appelle de la méthode getBalises()
+            $resultat = getBalises();// alors appelle de la méthode getBalises()
           
            // création de la réponse en JSON
            header('Content-Type: application/json');
-           echo json_encode($reponse, JSON_PRETTY_PRINT);
+           if($resultat !=false){
+            $retour['statut'] = 1;
+            $retour['message'] = "GET Balises";
+            $retour['donnees'] = $resultat;
+            
+        } else{
+            $retour['statut'] = 0;
+            $retour['message'] = "Echec de recuparation des donnees";
+            
+        }
+           echo json_encode($retour, JSON_PRETTY_PRINT);
         }
     }else{
         // Requête invalide
--- ffvlWeb/routeurMesure.php
+++ ffvlWeb/routeurMesure.php
@@ -11,27 +11,57 @@
         if(isset($_GET['date'])){
             $id = intval($_GET['id']);
             $date = $_GET['date'];
-            $reponse = getBaliseByDate($id, $date);
+            $resultat = getBaliseByDate($id, $date);
             
             // création de la réponse en JSON
             header('Content-Type: application/json');
-            echo json_encode($reponse, JSON_PRETTY_PRINT);
+            if($resultat !=false){
+                $retour['statut'] = 1;
+                $retour['message'] = "GET Mesure par date";
+                $retour['donnees'] = $resultat;
+                
+            } else{
+                $retour['statut'] = 0;
+                $retour['message'] = "Echec de recuparation des donnees";
+                
+            }
+            echo json_encode($retour, JSON_PRETTY_PRINT);
 
         }else if(isset($_GET['typeMesure'])){    // Si la propriété typeMesure existe 
                 $id = intval($_GET['id']);
                 $typeMesure = $_GET['typeMesure'];
-                $reponse = getBaliseMin($id, $typeMesure);
+                $resultat = getBaliseMin($id, $typeMesure);
                     
                 // création de la réponse en JSON
                 header('Content-Type: application/json');
-                echo json_encode($reponse, JSON_PRETTY_PRINT);
+                if($resultat !=false){
+                    $retour['statut'] = 1;
+                    $retour['message'] = "GET Mesure par type";
+                    $retour['donnees'] = $resultat;
+                    
+                } else{
+                    $retour['statut'] = 0;
+                    $retour['message'] = "Echec de recuparation des donnees";
+                    
+                }
+                echo json_encode($retour, JSON_PRETTY_PRINT);
         }else if(!empty($_GET['id'])){      // Si la propriété id existe
             $id = intval($_GET['id']);
-            $reponse = getBalise($id);    // alors appelle de la méthode getBalise($id)
+            $resultat = getBalise($id);    // alors appelle de la méthode getBalise($id)
 
             // création de la réponse en JSON
             header('Content-Type: application/json');
-            echo json_encode($reponse, JSON_PRETTY_PRINT);
+            if($resultat !=false){
+                $retour['statut'] = 1;
+                $retour['message'] = "GET Mesure";
+                $retour['donnees'] = $resultat;
+                
+            } else{
+                $retour['statut'] = 0;
+                $retour['message'] = "Echec de recuparation des donnees";
+                
+            }
+            echo json_encode($retour, JSON_PRETTY_PRINT);
 
         }else{
             // Requête invalide
